dcf80a23d13d8c1fdafb4f33a660f21441577d65
Mike Shen <michael.m.shen@gmail.com>
2023-04-25 01:01:27 +0000

    added files

---
+++ README.md
@@ -0,0 +1,25 @@
+# LAB - Class 26
+
+## Project: Intro to Django
+
+## Author: Mike Shen
+
+## Links and Resources
+N/A
+
+##  Setup
+per requirements.txt
+
+# How to initialize/run your application (where applicable)
+
+From root folder, run:
+
+python manage.py runserver
+
+## How to use your library (where applicable)
+
+## Tests
+
+From root folder, run:
+
+python manage.py test
---
+++ snacks/views.py
@@ -0,0 +1,18 @@
+from django.views.generic import TemplateView
+
+
+class HomePageView(TemplateView):
+    template_name = 'home.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["fruits"] = ['apple', 'banana', 'peach']
+        return context
+
+class AboutPageView(TemplateView):
+    template_name = 'about.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["characteristics"] = ['awesome', 'dapper', 'heroic']
+        return context
